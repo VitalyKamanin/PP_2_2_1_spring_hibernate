2ea5100f4c81ad71928554b95a9a3b0e2a174e85
VitalyKamanin <vitalykamanin@gmail.com>
2022-11-06 20:12:03 +0300

    initial commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
@@ -6,5 +6,7 @@ import java.util.List;
 
 public interface UserDao {
    void add(User user);
+
    List<User> listUsers();
+   User getUserByCar(int series, String model);
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,29 +1,44 @@
 package hiber.dao;
 
 import hiber.model.User;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.hibernate.sql.Select;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.TypedQuery;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
-
-   @Autowired
-   private SessionFactory sessionFactory;
+@PersistenceContext
+   private Session session;
 
    @Override
    public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
+      session.save(user);
    }
 
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
+      return session.createQuery("from User").getResultList();
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public User getUserByCar(int series, String model) {
+      User user;
+      try {
+         Query query = session.createQuery("from User where car.model= :model and car.series = :series");
+         query.setParameter("series", series);
+         query.setParameter("model", model);
+         user = (User) query.getSingleResult();
+      } catch (NoResultException e) {
+         user = null;
+      }
+      return user;
    }
 
 }
